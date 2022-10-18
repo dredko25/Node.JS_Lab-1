aed2164583a73f3004b57e03b87da2c7606caaab
dredko25 <dredko25@gmail.com>
2022-10-18 18:10:52 +0200

    run lint:fix

--- api/Hello_world.js
+++ api/Hello_world.js
@@ -1,5 +1,4 @@
-export default function handler(req, res) {
-    const { name = 'World' } = req.query;
-    return res.send(`Hello ${name}!`);
-  }
-  
+export default function handler (req, res) {
+  const { name = 'World' } = req.query
+  return res.send(`Hello ${name}!`)
+}

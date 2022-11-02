391b4b398582bea65dcbd5d1c4df1abee2fc7af1
dredko25 <dredko25@gmail.com>
2022-11-02 18:39:14 +0100

    fix format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,14 @@ module.exports = {
     node: true,
     es2021: true,
   },
-  extends: ['standard', 'plugin:sonarjs/recommended', "plugin:promise/recommended", "plugin:n/recommended", "plugin:import/recommended", 'prettier'],
+  extends: [
+    'standard',
+    'plugin:sonarjs/recommended',
+    'plugin:promise/recommended',
+    'plugin:n/recommended',
+    'plugin:import/recommended',
+    'prettier',
+  ],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',

7fe2a28253f6a09017effdc762d2e57b828bbdc9
dredko25 <dredko25@gmail.com>
2022-12-11 13:34:48 +0100

    fix format

--- src/router.js
+++ src/router.js
@@ -1,32 +1,30 @@
-import { readdir } from 'node:fs/promises';
-import * as path from 'node:path';
-import { fileURLToPath, pathToFileURL } from 'url';
+import { readdir } from 'node:fs/promises'
+import * as path from 'node:path'
+import { fileURLToPath, pathToFileURL } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const baseDir = path.join(__dirname, '/entity');
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const baseDir = path.join(__dirname, '/entity')
 
-
-
-const router = {};
-const isRouterFile = (name) => /\.router\.js$/.test(name);
+const router = {}
+const isRouterFile = (name) => /\.router\.js$/.test(name)
 
 async function loadRoutesDir(dirName, base) {
-    const relativePath = path.join(base, dirName);
-    const workDir = path.join(baseDir, relativePath);
-    const dir = await readdir(workDir, {withFileTypes: true});
+  const relativePath = path.join(base, dirName)
+  const workDir = path.join(baseDir, relativePath)
+  const dir = await readdir(workDir, { withFileTypes: true })
 
-    for (const dirent of dir) {
-        if (dirent.isDirectory()) {
-            await loadRoutesDir(dirent.name, path.join(base, dirName))
-        } else if (dirent.isFile() && isRouterFile(dirent.name)) {
-            let modulePath = pathToFileURL(path.join(workDir, dirent.name))
-            let module = await import(modulePath);
-            router[relativePath.replaceAll(path.sep, '/')] = {...module};
-        }
+  for (const dirent of dir) {
+    if (dirent.isDirectory()) {
+      await loadRoutesDir(dirent.name, path.join(base, dirName))
+    } else if (dirent.isFile() && isRouterFile(dirent.name)) {
+      const modulePath = pathToFileURL(path.join(workDir, dirent.name))
+      const module = await import(modulePath)
+      router[relativePath.replaceAll(path.sep, '/')] = { ...module }
     }
+  }
 }
 
 await loadRoutesDir('', path.sep)
 
-export { router };
+export { router }

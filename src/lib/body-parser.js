f000b5f15ab8e7d8aa21299d3b13b281db995907
dredko25 <dredko25@gmail.com>
2022-12-15 23:42:54 +0100

    add body-parser

---
+++ src/lib/body-parser.js
@@ -0,0 +1,27 @@
+
+const PROCESSED_CONTENT_TYPES = {
+    'text/html': (text) => text,
+    'text/plain': (text) => text,
+    'application/x-www-form-urlencoded': (data) => {
+        return Object.fromEntries(new URLSearchParams(data))
+    }
+}
+
+async function bodyParser(req) {
+    if(!req.headers['content-type']) {
+        return {}
+    }
+
+    let rawRequest = '';
+    for await (const chunk of req) {
+        rawRequest += chunk;
+    }
+
+    const contentType = req.headers['content-type'].split(';')[0];
+    const payload = PROCESSED_CONTENT_TYPES[contentType]?.(rawRequest) ?? {};
+
+    return { payload, rawRequest };
+}
+
+
+export { bodyParser }

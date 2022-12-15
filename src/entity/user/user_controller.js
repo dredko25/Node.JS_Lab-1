5afe05257fef8407d94f20e1ce434a35de13228e
annakurylo <anna.k.2003.g@gmail.com>
2022-12-16 01:27:21 +0200

    ADD: http-methods for controller

---
+++ src/entity/user/user_controller.js
@@ -0,0 +1,8 @@
+const findById = (req, res) =>
+    res.json({
+        id: Math.floor(Math.random() * 1000),
+        username: "testName",
+    });
+const createUser = (req, res, url, payload) => res.json({ status: "ok", ...payload });
+
+export { findById, createUser };

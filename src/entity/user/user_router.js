b114b36c676dd9bbfe187627ee7e671289f60366
dredko25 <dredko25@gmail.com>
2022-12-16 00:47:38 +0100

    fix format

--- src/entity/user/user_router.js
+++ src/entity/user/user_router.js
@@ -1,9 +1,10 @@
-import { findById, createUser } from './user.controller.js';
+import { findById, createUser } from './user_controller.js';
 
 class UserRouter {
     GET(...args) {
         return findById(...args);
     }
+
     POST(...args) {
         console.log(args);
         return createUser(...args);

7cc816c3881dfbc75b3f9016af3c5f799092bb06
dredko25 <dredko25@gmail.com>
2022-12-15 23:37:12 +0100

    fix format

--- src/server.js
+++ src/server.js
@@ -1,23 +1,25 @@
-import * as http from "node:http";
-import { router  } from "./router.js";
+import * as http from 'node:http'
+import { router } from './router.js'
 
 const server = http.createServer(async (req, res) => {
-    const url = new URL(req.url || "/", `https://${req.headers.host}`);
-    const routeModule = router[url.pathname].default ?? {};
-    const handler = routeModule[req?.method];
-});
+  const url = new URL(req.url || '/', `https://${req.headers.host}`)
+  const routeModule = router[url.pathname].default ?? {}
+  const handler = routeModule[req?.method]
+})
 
-server.on("clientError", (err, socket) => {
-    socket.end("HTTP/1.1 400 Bad Request\r\n\r\n");
-});
+server.on('clientError', (err, socket) => {
+    if (err) {
+        console.log(err.stack);
+    }
+  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n')
+})
 
-server.listen(process.env.PORT || 8000);
+server.listen(process.env.PORT || 8000)
 
-process.on("SIGINT", () => {
-    server.close((error) => {
-        if (error) {
-            console.error(error);
-            process.exit(1);
-        }
-    });
-});
+process.on('SIGINT', () => {
+  server.close((error) => {
+    if (error) {
+      throw new Error('Something bad happened!')
+    }
+  })
+})

df87ef6b15d745fd8cc6252b44471fab0e739240
dredko25 <dredko25@gmail.com>
2022-12-23 22:56:51 +0100

    add defaultHandler

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 import * as http from 'node:http'
-import { router } from './router.js'
+import { defaultHandler, router } from './router.js'
 import jsonRes from './middleware/jsonRes.js'
 import { bodyParser, buildChank } from './lib/body-parser.js'
 
 const server = http.createServer(async (req, res) => {
   const url = new URL(req.url || '/', `https://${req.headers.host}`)
   const routeModule = router[url.pathname].default ?? {}
-  const handler = routeModule[req?.method]
+  const handler = routeModule[req?.method] ?? defaultHandler
 
   const rawRequest = await buildChank(req)
   const { payload } = await bodyParser(req, rawRequest)
